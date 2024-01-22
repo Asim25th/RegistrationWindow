1d08fc775c77499ac503e5df5b951d98f92dbe69
Asim Rzaev <asim.rzaev@outlook.com>
2024-01-22 22:34:09 +0300

    New project published

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Окно_регистрации
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
